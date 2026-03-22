dceb066f9444cea47997705b1c958d5943784f45
dnkboussier-netizen <dnkboussier@gmail.com>
2026-03-22 19:31:55 +0100

    Update debug.js

--- netlify/functions/debug.js
+++ netlify/functions/debug.js
@@ -3,15 +3,9 @@ exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      // We tonen alleen de eerste 8 tekens zodat de key niet volledig zichtbaar is
       key_start: key ? key.substring(0, 8) : 'LEEG - NIET GEVONDEN',
       key_length: key ? key.length : 0,
       key_exists: !!key
     })
   };
 };
-```
-
-Nadat Netlify opnieuw heeft gedeployed, open je in je browser dit adres (vervang de sitenaam door die van jou):
-```
-https://lumina-ai-dani.netlify.app/.netlify/functions/debug
